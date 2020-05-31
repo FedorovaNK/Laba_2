6c46fc0262f17ecac8d9f222444a9e1d3ab11ac8
FedorovaNK <64545483+FedorovaNK@users.noreply.github.com>
2020-05-31 20:09:11 +0300

    Add files via upload

---
+++ WebS-Laba_2/SharedComponents/TorgObject.BusinessLogic/DomainObjects/Ports/IPechatProductRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Linq.Expressions;
+
+namespace TorgObject.DomainObjects.Ports
+{
+    public interface IReadOnlyPechatProductRepository
+    {
+        Task<Pechatproduct> GetPechatProduct(long id);
+       
+        Task<IEnumerable<Pechatproduct>> GetAllPechatProducts();
+
+        Task<IEnumerable<Pechatproduct>> QueryPechatProducts(ICriteria<Pechatproduct> criteria);
+
+    }
+
+    public interface IPechatProductRepository
+    {
+        Task AddPechatProduct(Pechatproduct pechatproduct);
+
+        Task RemovePechatProduct(Pechatproduct pechatproduct);
+
+        Task UpdatePechatProduct(Pechatproduct pechatproduct);
+    }
+}
---
+++ WebS-Laba_2/SharedComponents/TorgObject.PechatProductListQuery/ApplicationServices/GetMedPointListUseCase/IsDistrictCriteria.cs
@@ -0,0 +1,20 @@
+﻿using TorgObject.DomainObjects;
+using TorgObject.DomainObjects.Ports;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace TorgObject.ApplicationServices.GetMedPointListUseCase
+{
+    public class IsDistrictCriteria : ICriteria<MedPoint>
+    {
+        public string IsDistrict { get; }
+
+        public IsDistrictCriteria(string isDistrict)
+            => IsDistrict = isDistrict;
+
+        public Expression<Func<MedPoint, bool>> Filter
+            => (d => d.IsDistrict == IsDistrict);
+    }
+}
